76951641fd72f2a252296ab23e5a9fa3436d4d54
Shyamal kar <shyamalkar430@gmail.com>
2026-02-20 22:58:31 +0530

    Update dictionary_intro.py
    Corrected dictionary syntax and improved comments.

--- dictionary_intro.py
+++ dictionary_intro.py
@@ -1,7 +1,24 @@
 #Comma separated key-value pairs enclosed within {}
 #{key1:value1, key2:value2, ......}
 
-groceries = {"milk":60, "Biscuits":10, "Rice":100, "Bread":30}
-groceries["eggs: 10"] = 10 # Dictionary are mutable so change are accepted
-print(groceries)
-print(groceries[1])
+groceries = {
+    "milk: 10": 60,
+    "Biscuits": 10,
+    "Rice": 100,
+    "Bread": 30
+}
+
+print(groceries["milk: 10"])
+
+groceries = {"milk":60, #Dictionary never work with strate line . 
+             "Biscuits":10,
+             "Rice":100,
+             "Bread":30}
+groceries["eggs:"] = 10, # Dictionary are mutable so change are accepted, i hope you understand . 
+
+#If you want to access with key, then use 
+print(list(groceries.keys())[1])
+
+#If you want to access with values, then use value()
+
+print(list(groceries.values())[1])
