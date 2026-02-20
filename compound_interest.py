43499148a6032ce3f41d17efdb627bd7321b1a05
Shyamal kar <shyamalkar430@gmail.com>
2026-02-20 22:48:59 +0530

    Update compound_interest.py
    Added comments for clarity on principal input and calculation steps.

--- compound_interest.py
+++ compound_interest.py
@@ -4,11 +4,15 @@ Amount = P(1 * R/100) ** T
 ci = Amount - p 
 """
 
-principal = float(input("Enter principle amount:"))
+principal = float(input("Enter principle amount:")) # Float for decimal number
+
 rate = float(input("Enter interest rate: "))
+
 time = float(input("Enter time: "))
+
 #amount1 = principal * (1 + rate/100) ** time
-amount2 = principal * pow((1 + rate/100), time)
+
+amount2 = principal * pow((1 + rate/100), time) #First start on bracket and divide / 100 then complete stepby step
 print(round(amount2, 2))
 ci = amount2 - principal
 print("Compund interest",round (ci))
