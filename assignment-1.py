ede6072cc510a25cc74b9d3e429c57941c5f5eef
Shyamal kar <shyamalkar430@gmail.com>
2026-02-21 18:05:49 +0530

    Update assignment-1.py
    Updated formatting of mathematical operations list.

--- assignment-1.py
+++ assignment-1.py
@@ -2,10 +2,10 @@
 Problem Statement: Write a Python program that does the following:
 1.  Takes two numbers as input from the user.
 2.  Performs the basic mathematical operations on these two numbers:
- o	Addition
- o	Subtraction
- o	Multiplication
- o	Division
+ Addition
+ Subtraction
+ Multiplication
+ Division
 3.  Displays the results of each operation on the screen.
  Expected Output:
 The output should include the result of each operation performed, for example:
